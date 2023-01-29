e15d3f7d17789e8441443039e5afe8c4aaa5978e
SLD/VR <sladovir@mail.ru>
2023-01-29 18:51:15 +0300

    ver 1.0

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+# from loguru import logger
 
 from aiogram import Bot, Dispatcher
 
@@ -8,7 +9,9 @@ from handlers.user_handlers import register_user_handlers
 from handlers.other_handlers import register_other_handlers
 
 
-# Инициализируем логгер
+# Инициализируем логгер для отлавливания ошибок у пользователя
+# logger.add("debug.json", format="{time} {level} {message}",
+#            level="DEBUG", rotation="128 KB", compression="zip")
 logger = logging.getLogger(__name__)
 
 
@@ -17,3 +20,41 @@ def register_all_handlers(dp: Dispatcher) -> None:
     register_user_handlers(dp)
     register_other_handlers(dp)
 
+
+# Функция конфигерирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineni)d #% (levelname)-8s '
+        u'[%(asctime)s] - %(name)s - %(messages)s'
+    )
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher(bot)
+
+    # Регистрируем все хэндлеры
+    register_all_handlers(dp)
+
+    # Запускаем polling
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.close()
+
+
+if __name__ == '__main__':
+    try:
+        # Запускаем функцию main
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        # Выводим в консоль сообщение об ошибке,
+        # если получены исключения KeyboardInterrupt или SystemExit
+        logger.error('Bot stopped!')
