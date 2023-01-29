53a0b3eb658923dfd10fdc232d71d0e9e45a6cf0
SLD/VR <sladovir@mail.ru>
2023-01-29 21:07:06 +0300

    *refactoring

--- keyboards/menu_button.py
+++ keyboards/menu_button.py
@@ -5,8 +5,7 @@ from lexicon.lexicon_ru import LEXICON_RU_COMMAND
 # Функция для настройки кнопки Menu бота
 async def set_main_menu(dp: Dispatcher):
     main_menu_commands = []
-    for command in LEXICON_RU_COMMAND:
-        print(command)
-        main_menu_commands.append(types.BotCommand(command=f'{command}', description=f'{LEXICON_RU_COMMAND[command]}'))
+    for command, description in LEXICON_RU_COMMAND.items():
+        main_menu_commands.append(types.BotCommand(command=f'{command}', description=f'{description}'))
     await dp.bot.set_my_commands(main_menu_commands)
 
